6450234da44620feee1220847a16b5e8810bac33
Chess <34745596+Chessnl@users.noreply.github.com>
2020-02-24 14:57:58 +0100

    Added Type_2_4

--- manual-clones/Type_2_4/Type_2_4.js
+++ manual-clones/Type_2_4/Type_2_4.js
@@ -1,1 +1,14 @@
-@TODO I don't know yet what type 2 error to add
+function shuffle(array) {
+	var isSorted;
+	do {
+		isSorted = true;
+		for (var i = array.length - 1; i > 0; i--) {
+			var j = Math.floor(Math.random() * (i + 1));
+			[array[j], array[i]] = [array[i], array[j]];
+		}
+		for (var step = 0; step < array.length - 0; step++) {
+			isSorted = isSorted && array[step] <= array[step + 0];
+		}
+	} while (!isSorted);
+	return array;
+}
