31bf6bae483a195a2b48882951a2d5a7983e6e90
Chess <34745596+Chessnl@users.noreply.github.com>
2020-02-26 23:13:10 +0100

    Updated python scripts

--- process.py
+++ process.py
@@ -18,17 +18,24 @@ def main():
 
         print(f'Processed {line_count} lines.')
 
-    i = 1
-    l = len(releases) * (len(releases) - 1) / 2
-    for i1, release1 in enumerate(releases[:4]):
-        for i2, release2 in enumerate(releases[:4]):
+    # @TODO im not sure whether this function made sense.
+    # Side-note, I was not 100% sober when writing this. Have fun future me Look at all that indentation booiii
+    matchedValues = []
+    for i1, release1 in enumerate(releases):
+        matchedValues.append([])
+        for i2, release2 in enumerate(releases):
             if i1 < i2:
-                i += 1
-                print(f"Processing {i} out of {l}")
                 with open(f"./out/return/{release1['tag']}-{release2['tag']}.txt") as handle:
                     dict = json.loads(handle.read())
-                    print(dict)
-                    ## @TODO currently doesn't do anything compare the files
+                    overlap = 0
+                    for match in dict:
+                        version = match['instances'][0]['path'].split('/')[2]
+                        for instance in match['instances']:
+                            if instance['path'].split('/')[2] != version:
+                                overlap += instance['lines'][1] - instance['lines'][0] * len(match['instances'])
+                                break
+                    matchedValues[i1].append(overlap)
+    print(matchedValues)
 
 
 if __name__ == "__main__":
