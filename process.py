2e66e1bd224b309016e6517daabe40896c66b516
Square Play'n <pietervoors@hotmail.com>
2020-03-03 11:42:58 +0100

    Calculate size of code overlap for any two versions with intervals

--- process.py
+++ process.py
@@ -18,25 +18,52 @@ def main():
 
         print(f'Processed {line_count} lines.')
 
-    # @TODO im not sure whether this function made sense.
-    # Side-note, I was not 100% sober when writing this. Have fun future me Look at all that indentation booiii
-    matchedValues = []
+    matches = dict()
     for i1, release1 in enumerate(releases):
-        matchedValues.append([])
+        release1 = release1['tag']
+        matches[i1] = dict()
         for i2, release2 in enumerate(releases):
+            release2 = release2['tag']
             if i1 < i2:
-                with open(f"./out/return/{release1['tag']}-{release2['tag']}.txt", encoding='cp850') as handle:
-                    dict = json.loads(handle.read())
+                with open(f"./out/return/{release1}-{release2}.txt", encoding='cp850') as handle:
+                    jsInspectResults = json.loads(handle.read())
+                    intervals = {}
 
-                    overlap = 0
-                    for match in dict:
-                        version = match['instances'][0]['path'].split('/')[2]
+                    # Go over all duplication matches
+                    for match in jsInspectResults:
+                        # Check to make sure they are not just matches only within the same version
+                        includesR1 = False
+                        includesR2 = False
                         for instance in match['instances']:
-                            if instance['path'].split('/')[2] != version:
-                                overlap += (instance['lines'][1] - instance['lines'][0]) * len(match['instances'])
-                                break
-                    matchedValues[i1].append(overlap)
-    print(matchedValues)
+                            version = instance['path'].split('/')[2]
+                            if version == release1:
+                                includesR1 = True
+                            if version == release2:
+                                includesR2 = True
+
+                        # Only consider matches between different versions
+                        if (not includesR1) or (not includesR2):
+                            break
+
+                        # Add the new intervals of these matches
+                        for instance in match['instances']:
+                            file = instance['path']
+                            version = instance['path'].split('/')[2]
+                            start = int(instance['lines'][0])
+                            end = int(instance['lines'][1])
+                            if file not in intervals.keys():
+                                intervals[file] = []
+
+                            # TODO Handle intervals gracefully instead of blatantly adding
+                            intervals[file].append({'start': start, 'end': end})
+
+                    overlapSize = 0
+                    for fileIntervals in intervals.values():
+                        for interval in fileIntervals:
+                            overlapSize += interval['end'] - interval['start']
+
+                    matches[i1][i2] = overlapSize
+    print(matches)
 
 
 if __name__ == "__main__":
