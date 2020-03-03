676894dc000c4a3a831f5fea1f52d93a8731069f
Square Play'n <pietervoors@hotmail.com>
2020-03-03 09:50:54 +0100

    Fix missing brackets

--- compare.py
+++ compare.py
@@ -27,7 +27,7 @@ def main():
         for i2, release2 in enumerate(releases):
             if i1 < i2:
                 i += 1
-                command = f"jsinspect -r json --ignore \".*/outro\.js|.*/intro\.js\" ./jquery-data/{release1['tag']}/src ./jquery-data/{release2['tag']}/src > ../out/return/{release1['tag']}-{release2['tag']}.txt"
+                command = f"jsinspect -r json --ignore \".*/outro\.js|.*/intro\.js\" ./jquery-data/{release1['tag']}/src ./jquery-data/{release2['tag']}/src > ../out/return/{release1['tag']}-{release2['tag']}.json"
                 print(f"Executing {i} out of {l}")
                 os.system(command)
 
--- process.py
+++ process.py
@@ -25,14 +25,15 @@ def main():
         matchedValues.append([])
         for i2, release2 in enumerate(releases):
             if i1 < i2:
-                with open(f"./out/return/{release1['tag']}-{release2['tag']}.txt") as handle:
+                with open(f"./out/return/{release1['tag']}-{release2['tag']}.txt", encoding='cp850') as handle:
                     dict = json.loads(handle.read())
+
                     overlap = 0
                     for match in dict:
                         version = match['instances'][0]['path'].split('/')[2]
                         for instance in match['instances']:
                             if instance['path'].split('/')[2] != version:
-                                overlap += instance['lines'][1] - instance['lines'][0] * len(match['instances'])
+                                overlap += (instance['lines'][1] - instance['lines'][0]) * len(match['instances'])
                                 break
                     matchedValues[i1].append(overlap)
     print(matchedValues)
